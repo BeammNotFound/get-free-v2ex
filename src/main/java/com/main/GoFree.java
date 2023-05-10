76aba2f60dcd29298fce286e46bdf3e5d8260730
beammnotfound <#include0613>
2023-05-11 02:20:58 +0800

    🌈

--- src/main/java/com/main/GoFree.java
+++ src/main/java/com/main/GoFree.java
@@ -67,7 +67,6 @@ public class GoFree {
     @Scheduled(cron = "0 0 */6 * * ?")
     private static void saveData() {
         log.info("更新data.dat");
-        System.out.println("------------");
         saveListMap(getFree());
     }
 
